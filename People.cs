d33e6c0d0927397ed93e025cfdcf92beaf844f4d
Student14 <Student14@LAB3-WS14>
2024-09-19 10:59:20 +0300

    Add project files.

---
+++ People.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRclassOhados
+{
+    internal class People
+    {
+        private string id;
+        private string telephone;
+        private string adress;
+        private string name;
+
+        public People() { }
+        public People(string id,string telphone,string adress,string name)
+        {
+          this.id = id;
+          this.telephone = telphone;
+          this.adress = adress;
+          this.name = name;
+        }
+
+
+        //get 
+
+        public string getId() { return id; }
+        public string getTelephone() {  return telephone; }
+        public string getAdress() {  return adress; }
+        public string getName() { return name; }
+        //set
+
+        public void SetID(string id) { this.id = id;}
+        public void SetTelephone(string telephone) { this.telephone = telephone;}
+        public void SetAdress(string adress) { this.adress  =adress;}
+        public void SetName(string name) { this.name = name;}
+
+
+        //פעולות שונות
+
+       
+        //הדפסה כללית
+        public override string ToString()
+        {
+            return this.id + " ," + this.telephone + " ," + this.adress + ", " + this.name;
+        }
+
+        //הדפסת כתובת
+        public string PrintNameAdress()
+        {
+           
+            return this.adress + " ," + this.name;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRclassOhados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            People noam = new People("202342","0505124","ofarim2","noam");
+
+            Console.WriteLine(noam.ToStringA());
+
+
+
+
+        }
+    }
+
+   
+}
